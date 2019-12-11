e075215d4256d7a547107523f5e74bc07229a86f
bizar4ik-b3 <bizar4ik.b3@gmail.com>
2019-12-11 13:07:46 +0200

    Templating sidebar. created component

--- src/app/system/system.module.ts
+++ src/app/system/system.module.ts
@@ -7,11 +7,12 @@ import { HistoryPageComponent } from './history-page/history-page.component';
 import { PlanningPageComponent } from './planning-page/planning-page.component';
 import { RecordsPageComponent } from './records-page/records-page.component';
 import { SystemComponent } from './system.component';
+import { SidebarComponent } from './shared/components/sidebar/sidebar.component';
 
 
 
 @NgModule({
-  declarations: [BillPageComponent, HistoryPageComponent, PlanningPageComponent, RecordsPageComponent,SystemComponent],
+  declarations: [BillPageComponent, HistoryPageComponent, PlanningPageComponent, RecordsPageComponent,SystemComponent, SidebarComponent],
   imports: [
     CommonModule,
     SharedModule,
