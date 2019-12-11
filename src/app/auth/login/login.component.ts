2a8e0499556bf6ca09c40925b68446fe772345f1
bizar4ik-b3 <bizar4ik.b3@gmail.com>
2019-12-11 13:06:56 +0200

    Redirect to bill page after login

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -50,7 +50,7 @@ export class LoginComponent implements OnInit {
             this.message.text = "";
             window.localStorage.setItem("user", JSON.stringify(user));
             this.authService.login();
-           // this.router.navigate(['']);
+            this.router.navigate(['/system','bill']);
             this.user = user;
           } else {
             this.showMessage({
